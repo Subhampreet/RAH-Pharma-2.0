7a4d181935a8d90f60fe234f21435f39b7ddb7af
debidattasuryaprakash <suryaprakash.d20@gmail.com>
2021-09-30 03:40:48 +0000

    copyright removed

--- src/landingPage/components/footer.js
+++ src/landingPage/components/footer.js
@@ -76,17 +76,6 @@ function footer() {
                     </div>
                 </div>
             </div>
-            <div className="footer-bottom pt-25 pb-20">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-12">
-                            <div className="footer-copyright text-center">
-                                <p className="white-color">Copyright by@ BasicTheme - 2019</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             </div> 
         </div>
     )
