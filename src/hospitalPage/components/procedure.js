7bb5e562a602580d4a6b5f6bc74a21487980ffad
Sai Soumyak Nanda <saisoumyaknanda@gmail.com>
2021-10-08 12:20:23 +0000

    Procedure Section

---
+++ src/hospitalPage/components/procedure.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '../styles/procedure.css';
+
+function Procedure() {
+    return (
+        <div className="procedure-cont">
+            <div className="procedure-body">
+                <div className="procedure-title">
+                    Procedure
+                </div>
+                <div className="procedure-title-text">
+                    A stepwise guide to the procedural flow of  treartment followed by the hospital.   
+                </div>
+                <div className="procedures">
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     <div className="procedure-item">
+                         <img src={require('../img/rah-assets/folder.png').default}/>
+                     </div>
+                     
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Procedure
