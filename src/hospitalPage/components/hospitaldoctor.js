82b02e5e3899f9f69b6ddac3daee562e55bd67f7
debidattasuryaprakash <suryaprakash.d20@gmail.com>
2021-10-09 02:24:38 +0000

    content box changes

--- src/hospitalPage/components/hospitaldoctor.js
+++ src/hospitalPage/components/hospitaldoctor.js
@@ -15,27 +15,27 @@ function HospitalDoctor() {
              </div>
                    <div className="hosdoc-doctor-section">
                    <div className="row h4service-active">
-                    <div className="col-xl-4 col-lg-6 col-md-6 h4service-main">
+                    <div className="col-xl-5 col-lg-6 col-md-6 h4service-main">
                         <div className="h4service-box white-bg mb-30">
                             <div className="service-thumb service1 pos-rel mb-0">
                                 <img src={require("../img/rah-assets/doctor.png").default} alt=""  />
                             </div>
                             <div className="service-content h4services-content" >
-                            <h3 className="f-700"><a href="#">Sum Hospital</a></h3>
+                            <h3 className="f-700"><a href="#">Dr. Srinivas Reddy</a></h3>
                                 <div className="service-content-img-div">
                                 <img className="h4-services-content-img" src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 location f-600">4 kms away</p>
+                                <p className="mb-20 location f-600">30yrs Clinical Experience.</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600">400 Hospital Beds</p>
+                                <p className="mb-20 f-600">Best Doctor Award 2020</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600 number">+91 9000000000</p>
+                                <p className="mb-20 f-600">People’s Choice Award</p>
                                 </div>
                                 
-                                <a className="service-link" href="services-details.html">Explore  <img src= {require("../img/rah-assets/folder.png").default} alt=""/></a>
+                                <a data-animation="fadeInLeft" data-delay=".6s" href="contact.html" className="btn btn-icon btn-icon-green ml-0"><span><img src={require('../img/rah-assets/phonecall Green.png').default} alt="" /></span>Check Availability</a>
                             </div>
                         </div>
                         </div>
@@ -47,21 +47,21 @@ function HospitalDoctor() {
                                 <img src={require("../img/rah-assets/doctor.png").default} alt=""  />
                             </div>
                             <div className="service-content h4services-content" >
-                            <h3 className="f-700"><a href="#">Sum Hospital</a></h3>
+                            <h3 className="f-700"><a href="#">Dr. Srinivas Reddy</a></h3>
                                 <div className="service-content-img-div">
                                 <img className="h4-services-content-img" src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 location f-600">4 kms away</p>
+                                <p className="mb-20 location f-600">30yrs Clinical Experience.</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600">400 Hospital Beds</p>
+                                <p className="mb-20 f-600">Best Doctor Award 2020</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600 number">+91 9000000000</p>
+                                <p className="mb-20 f-600">People’s Choice Award</p>
                                 </div>
                                 
-                                <a className="service-link" href="services-details.html">Explore  <img src= {require("../img/rah-assets/folder.png").default} alt=""/></a>
+                                <a data-animation="fadeInLeft" data-delay=".6s" href="contact.html" className="btn btn-icon btn-icon-green ml-0"><span><img src={require('../img/rah-assets/phonecall Green.png').default} alt="" /></span>Check Availability</a>
                             </div>
                         </div>
                         </div>
@@ -73,21 +73,21 @@ function HospitalDoctor() {
                                 <img src={require("../img/rah-assets/doctor.png").default} alt=""  />
                             </div>
                             <div className="service-content h4services-content" >
-                            <h3 className="f-700"><a href="#">Sum Hospital</a></h3>
+                            <h3 className="f-700"><a href="#">Dr. Srinivas Reddy</a></h3>
                                 <div className="service-content-img-div">
                                 <img className="h4-services-content-img" src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 location f-600">4 kms away</p>
+                                <p className="mb-20 location f-600">30yrs Clinical Experience.</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600">400 Hospital Beds</p>
+                                <p className="mb-20 f-600">Best Doctor Award 2020</p>
                                 </div>
                                 <div className="service-content-img-div">
                                 <img src= {require("../img/rah-assets/folder.png").default} alt=""/>
-                                <p className="mb-20 f-600 number">+91 9000000000</p>
+                                <p className="mb-20 f-600">People’s Choice Award</p>
                                 </div>
                                 
-                                <a className="service-link" href="services-details.html">Explore  <img src= {require("../img/rah-assets/folder.png").default} alt=""/></a>
+                                <a data-animation="fadeInLeft" data-delay=".6s" href="contact.html" className="btn btn-icon btn-icon-green ml-0"><span><img src={require('../img/rah-assets/phonecall Green.png').default} alt="" /></span>Check Availability</a>
                             </div>
                         </div>
                         </div>
