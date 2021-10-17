a050ce4e4e319eda8f020f5c003795695d56031a
Ayushmina-20 <ayushminanayak20@gmail.com>
2021-10-16 22:07:39 -0700

    cards scrolling

--- src/hospitalPage/components/hospitaldoctor.js
+++ src/hospitalPage/components/hospitaldoctor.js
@@ -87,6 +87,32 @@ export default function Doctors() {
             <div className="text8">Check Availability</div>
           </div>
         </div>
+        <div className="card8">
+          <div className="image8">
+            <img
+              src="https://i.ibb.co/QQWp5QH/usman-yousaf-p-Trhfmj2j-DA-unsplash-1-1.png"
+              alt=""
+            />
+          </div>
+          <div className="name8">Dr. Srinivas Reddy</div>
+          <div className="desc8">
+            <li className="li">
+              <span>30yrs Clinical Experience.</span>
+            </li>
+            <li className="li">
+              <span>30yrs Clinical Experience.</span>
+            </li>
+            <li className="li">
+              <span>30yrs Clinical Experience.</span>
+            </li>
+          </div>
+          <div className="button8">
+            <div className="icon8">
+              <ArrowForwardIcon />
+            </div>
+            <div className="text8">Check Availability</div>
+          </div>
+        </div>
       </div>
     </div>
   );
