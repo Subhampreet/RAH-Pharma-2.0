239a2d67c63ad7ed4e4da57bd270e3b11e9a5a7a
Ayushmina-20 <ayushminanayak20@gmail.com>
2021-10-21 10:42:28 -0700

    changes

--- src/Admin/users.js
+++ src/Admin/users.js
@@ -1,18 +1,37 @@
 import * as React from "react";
-import { List, Datagrid,NumberField, TextField, EmailField } from 'react-admin';
+import { List, Datagrid,NumberField, TextField, EditButton ,SimpleList} from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
 
-export const UserList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="Hospital Name" />
+export const UserList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList
+                   
+                primaryText={record =>record.HospitalName}
+                secondaryText={record => record.ICU }
+                tertiaryText={record=>record.TotalSeats}
+
+
+                    
+                    
+                    
+                />
+            ) : (
+                <Datagrid>
+                   <TextField source="id" />
+            <TextField source="HospitalName"/>
             <TextField source="Hospital Type" />
             <NumberField source="Contact Number"/>
             <TextField source="Address"/>
-            <NumberField source="Total Seats"/>
+            <NumberField source="TotalSeats"/>
             <NumberField source="ICU"/>
             <TextField source="Departments"/>
             <TextField source="Facilities"/>
-        </Datagrid>
-    </List>
-);
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
